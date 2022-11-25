66b42ebcda839985ad730da9d535ea37d4207dc8
Franck Mugisho <fbmugisho@gmail.com>
2022-11-25 16:40:07 -0500

    adding api client functionality to frontend

---
+++ cueats.js
@@ -0,0 +1,32 @@
+const apiKey = "DBRcedm5J4a8k4wxtwcEJ3vltGN7knEU8GLHOnLn";
+const apiEndPoint = "https://2w4dq70fjc.execute-api.us-east-1.amazonaws.com/v1";
+
+window.onload = function(){
+    $("#search_order").click(function(){
+        // Search button has been clicked. Do something!
+        const searchItem = $("#q").val();
+        console.log(searchItem);
+        $("#q").val(''); // clearing up the search bar.
+        $("#q").focus(); // focus cursor on the search bar.
+
+        const apigClient = apigClientFactory.newClient({
+            apiKey: apiKey
+        });
+        const params = {
+            "q": input
+        };
+        const body = {};
+        const additionalParams = {
+            headers:{},
+            queryParams: {}
+        };
+
+        apigClient.searchGet(params, body, additionalParams)
+        .then(function(result) {
+            // Some result found. Do something
+
+        }).catch(function(result) {
+            // Some exception occurred. Handle here
+        });
+    })
+}
