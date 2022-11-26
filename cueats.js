22d4aa0f902269bb45426aaaa3b4c78c16054a36
Franck Mugisho <fbmugisho@gmail.com>
2022-11-26 15:09:01 -0500

    improved front-end with place holders for suggestions and browsing by locations

--- cueats.js
+++ cueats.js
@@ -23,9 +23,11 @@ window.onload = function(){
 
         apigClient.searchGet(params, body, additionalParams)
         .then(function(result) {
+            // TODO:    `FMugisho implement
             // Some result found. Do something
 
         }).catch(function(result) {
+            // TODO:    `FMugisho implement
             // Some exception occurred. Handle here
         });
     })
