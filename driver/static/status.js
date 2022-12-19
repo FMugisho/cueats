85e425b6855e1477cff8cff4961394d4e432474d
Franck Mugisho <fbmugisho@gmail.com>
2022-12-19 12:48:51 -0500

    cleaned up status scripts

--- driver/static/status.js
+++ driver/static/status.js
@@ -5,12 +5,11 @@ $(document).ready(function() {
     console.log( "ready!" );
     $("#key1").click(function() {
         // TODO FMugisho: add class big dot to span on click
-        alert("key 1 clicked");
+        // Order received --- do something
         const today = new Date();
         // TODO FMugisho: add class big dot to span on click
         $("#key1").addClass("big dot"); // marking the current status
         $("#val1").text(today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate() +' '+today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds()); // assign datetime as status text
-        $("#val1-2").val("Order received");
 
         // send position because we have received the order
         watchId = navigator.geolocation.watchPosition(function(position) {
@@ -39,8 +38,7 @@ $(document).ready(function() {
           
     });
     $("#key2").click(function() {
-        // do something
-        alert("key 2 clicked");
+        // Accepted order --- do something
         const today = new Date();
         // TODO FMugisho: add class big dot to span on click
         $("#key2").addClass("big dot"); // marking the current status
@@ -49,33 +47,27 @@ $(document).ready(function() {
 
     }); 
     $("#key3").click(function() {
-        // do something
-        alert("key 3 clicked");
+        // Order picked up from vendor --- do something
         const today = new Date();
         // TODO FMugisho: add class big dot to span on click
         $("#key3").addClass("big dot"); // marking the current status
         $("#val3").text(today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate() +' '+today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds()); // assign datetime as status text
-        $("#val3-2").val("Order picked up from vendor");
 
     }); 
     $("#key4").click(function() {
-        // do something
-        alert("key 4 clicked");
+        // Order out for delivery --- do something
         const today = new Date();
         // TODO FMugisho: add class big dot to span on click
         $("#key4").addClass("big dot"); // marking the current status
         $("#val4").text(today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate() +' '+today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds()); // assign datetime as status text
-        $("#val4-2").val("Order out for delivery");
 
     });
     $("#key5").click(function() {
-        // do something
-        alert("key 5 clicked");
+        // Delivered --- do something
         const today = new Date();
         // TODO FMugisho: add class big dot to span on click
         $("#key5").addClass("big dot"); // marking the current status
         $("#val5").text(today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate() +' '+today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds()); // assign datetime as status text
-        $("#val5-2").val("Delivered");
         navigator.geolocation.clearWatch(watchId); // stop tracking driver's position
         console.log("we're done tracking your position");
 
