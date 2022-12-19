ae66ed54770f5fc766b9bdb80d5225fdf5c3da58
Franck Mugisho <fbmugisho@gmail.com>
2022-12-18 20:51:48 -0500

    refactoring dir structure + add delivery page that plots map

---
+++ static/checkout.js
@@ -0,0 +1,68 @@
+const apigClient = apigClientFactory.newClient({apiKey: "DBRcedm5J4a8k4wxtwcEJ3vltGN7knEU8GLHOnLn"});
+
+$(document).ready(function(){
+  const urlSearchParams = new URLSearchParams(window.location.search);
+  const orderDetails = Object.fromEntries(urlSearchParams.entries());
+  console.log("the order details params are " + JSON.stringify(orderDetails));
+  populateCart(orderDetails);
+
+  $("#placeorder").click(function(){
+    console.log("the final place order button has been clicked ... ");
+    const formResponse = parseForm();
+    const params = {
+      "Content-Type": "application/json",
+    };
+    const body = {
+      "messages": [
+        {
+          "type": "",
+          "unstructured": {
+            "order_name": JSON.stringify(orderDetails),
+            "order_price": "15",
+            "user_email": formResponse[0],
+            "location": "CU Dining", // TODO FMugisho: Parse real location
+            "details": formResponse[1],
+            "timestamp": ""
+          }
+        }
+      ]
+    };
+    const additionalParams = {
+      headers: {
+        "Content-Type": "application/json"
+      },
+    };
+    const result = apigClient.placeOrderPost(params, body, additionalParams).then(function(result){
+      console.log("here is the result from the post request: " + result);
+    }).catch(function (result) {
+      console.log("in the exception block!");
+      console.log(result);
+    });
+    
+  });
+})
+
+function populateCart(items){
+    const totalPrice = 15; // assume customers will be billed by the average money value of a swipe.
+    const discount = 0; // TODO: adjust discount value if applicable.
+    let itemCount = 0;
+    for (const [key, value] of Object.entries(items)){
+        itemCount += 1;
+        $("#cart-items").append("<li class=\"list-group-item d-flex justify-content-between lh-condensed\"><div><h6 class=\"my-0\">" + key + "</h6>" + "<small class=\"text-muted\">Lorem Ipsum</small></div> <span class=\"text-muted\">Qty: " + value + "</span> </li>");
+    }
+    $("#cart-count").append(itemCount);
+    $("#cart-items").append("<li class=\"list-group-item d-flex justify-content-between bg-light\"><div><h6 class=\"my-0\">" + "Promo code" + "</h6>" + "<small class=\"text-muted\">TBD</small></div> <span class=\"text-success\">" + discount + "</span> </li>");
+    $("#cart-items").append("<li class=\"list-group-item d-flex justify-content-between\"><span>Total (USD)</span> <strong> $" + (totalPrice - discount) + "</strong> </li>");
+}
+
+function parseForm(){
+  const details = {
+    "username": $("#username").val,
+    "firstname": $("#firstName").val,
+    "lastname": $("#lastName").val,
+    "address": $("#address").val(),
+  }
+  const emailAddr = $("#email").val();
+  // TODO FMugisho: parse other element like name, address, etc
+  return [emailAddr, JSON.stringify(details)];
+}
---
+++ static/home.js
@@ -0,0 +1,109 @@
+const apigClient = apigClientFactory.newClient()
+const images = [
+    "https://cu-eats-client-front-end.s3.amazonaws.com/static-photos/Roaree-73v2.jpg",
+    "https://cu-eats-client-front-end.s3.amazonaws.com/static-photos/Campus-dining.jpg",
+    "https://cu-eats-client-front-end.s3.amazonaws.com/static-photos/columbia-jj-thumb-3.jpg"
+]
+let cartItems = {}; // global variable to store shopping cart information.
+let i = 0
+
+function make_location_card(image_src, title_text, description_text) {
+    const card = $("<div class='col-sm card location-card'>")
+    const img = $("<img>")
+    img.attr('src', image_src)
+    const body = $("<div class='card-body'>")
+    const title = $("<h5 class='card-title'>")
+    title.html(title_text)
+    body.append(title)
+    body.append($("<hr class='my-4'>"))
+    const description = $("<p class='card-text' style='text-align:center'>")
+    description.html(description_text)
+    body.append(description)
+
+    card.append(img, body)
+
+    return card
+}
+
+function make_item_card(menu_item) {
+    const card = $("<div class='col-sm-3 card'>")
+    const img = $("<img class='card-img-top'>")
+    img.attr("src", menu_item.image_url)
+    const card_body = $("<div class='card-body'>")
+    const title = $("<h5 class='card-title'>")
+    title.html(menu_item.title)
+    const card_description = $("<p class='card-text' style='text-align: center'>")
+    card_description.html(menu_item.description)
+    const add_to_cart_btn = $("<a href='#' class='btn btn-outline-primary btn-rounded cartbutton' data-mdb-ripple-color='dark'>")
+    add_to_cart_btn.html("Add to cart")
+
+    card_body.append(title, $("<hr class='my-4'>"), card_description, add_to_cart_btn)
+
+    card.append(img, card_body)
+
+    return card
+}
+
+$(document).ready(function () {
+    console.log("home.js loaded");
+    const result = apigClient.getMenuGet().then(function (result) {
+        console.log(result.data)
+        $.each(result.data, function (location_id, cur_location) {
+            let cur_location_card = make_location_card(
+                images[i++ % images.length], // TODO: use real image
+                cur_location.location_name,
+                cur_location.location_name
+            )
+            $("#locations-container").append(cur_location_card)
+
+            // When the card is clicked, we show the menu for that location
+            cur_location_card.click(function () {
+                const items_container = $("#items-container")
+                items_container.empty()
+
+                $.each(cur_location.location_details, function (cur_meal, stations) {
+                    // TODO: show the meal (like breakfast, lunch etc...)
+                    // const meal_description = $("<div class='col-sm-12>'")
+                    // meal_description.html(cur_meal)
+                    // items_container.append(meal_description)
+                    // items_container.append("<br>")
+
+                    $.each(stations, function (cur_station, menu_items) {
+                        // TODO: Show the station (like main line, grill etc.)
+                        // const station_description = $("<h3>")
+                        // station_description.html(cur_station)
+                        // items_container.append(station_description)
+                        // items_container.append("<br>")
+
+                        $.each(menu_items, function (j, cur_menu_item) {
+                            const menu_item = {
+                                "image_url" : "image_url", // TODO
+                                "name" : cur_menu_item,
+                                "description" : "Delicious food cooked to perfection" // TODO
+                            }
+                            const card = make_item_card(cur_menu_item)
+                            $("#items-container").append(card);
+                            card.click(function(){
+                                const item = $(card).text().replace("Add to cart", "");
+                                // TODO FMugisho: Right now, any click can add item to cart items. This should be avoided. Only clicks to add to cart button should be accounted for.
+                                if (item in cartItems){
+                                    cartItems[item] = cartItems[item] + 1;
+                                } else {
+                                    cartItems[item] = 1;
+                                }
+                            })
+                        })
+                    })
+                })
+            })
+        })
+    }).catch(function (result) {
+        console.log(result)
+    })
+
+    $("#checkout").click(function(){
+        const queryString = Object.keys(cartItems).map(key => key + '=' + cartItems[key]).join('&');
+        window.location.href = "checkout.html" + "?" + queryString;
+    })
+    console.log("results: ", result)
+})
